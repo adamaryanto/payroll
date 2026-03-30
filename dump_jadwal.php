2b107de727a1cc398434a3ae89baf4120db7d490
Rivkychao <rivky69ps@gmail.com>
2026-03-30 20:57:14 +0700

    before final

--- dump_jadwal.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "koneksi.php";
-$q = $koneksi->query("SELECT * FROM tb_jadwal");
-while ($row = $q->fetch_assoc()) {
-    print_r($row);
-}
-?>
--- dump_schema.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "koneksi.php";
-$query = $koneksi->query("SHOW COLUMNS FROM tb_realisasi_detail");
-$output = "";
-while ($row = $query->fetch_assoc()) {
-    $output .= $row['Field'] . " (" . $row['Type'] . ")\n";
-}
-file_put_contents("schema_output.txt", $output);
-?>
